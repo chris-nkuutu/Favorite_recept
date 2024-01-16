d855d0fdb13798562dd740a1fc5a459a43bfd248
Chris_nkuutu <nkuutugeorge@gmail.com>
2024-01-16 23:24:14 +0100

    Created project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n-_-_-_-Favorite Recept_-_-_-_-\n");
+        System.out.println("1: Pancakes: \n");
+        System.out.println("Ingredients: Eggs, butter, baking powder, vanila creme, sugar\n");
+        System.out.println("2: Semla: \n");
+        System.out.println("Ingredients: Eggs, creme, baking powder, vanila, sugar\n");
+        System.out.println("3: Våfflor: \n");
+        System.out.println("Ingredients: Eggs, creme, baking powder, vanila, sugar");
+    }
+}
